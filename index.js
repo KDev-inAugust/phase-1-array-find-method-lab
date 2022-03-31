f4c9637fc5e1b7d4edde0bba410b695e2f0dc7b4
Kane Mathis <admin@kanemathis.com>
2022-03-30 17:31:22 -0700

    commit tree 31a73e05f0d329514f9591aed88e740516496205

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin (record){
+    for (let elem in record){
+  if (record[elem].result === "W"){
+      return (record[elem].year)
+  }
+    }
+}
+
+record.find(superbowlWin);
